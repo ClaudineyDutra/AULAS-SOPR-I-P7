f4a1919d53d82e0610c740d2e2709a18c7eafe73
Claudiney <claudineygui@gmail.com>
2022-05-24 11:21:37 -0300

    Calculado Geometrica

---
+++ CalculadoraGeometrica/cgi-bin/geo_funcs.py
@@ -0,0 +1,14 @@
+#C:\\Users\\Lenovo\\PycharmProjects\\aulas-sopr-I-p7\\CalculadoraGeometrica\\venv
+
+def print_header(title):
+    print("Content-type:text/html\r\n\r\n")
+    print("""<html>
+                <head>
+                    <meta charset="UTF-8">
+                    <title>{}</title>
+                </head>
+                <body>""".format(title))
+
+
+def print_footer():
+    print("</body></html>")
